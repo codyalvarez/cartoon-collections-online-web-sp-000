a91eb1b19faca5da2b67eec29f4641479db25be7
Cody Alvarez <cody_alvarez19@outlook.com>
2021-03-03 01:47:32 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -5,7 +5,7 @@ def roll_call_dwarves(dwarves)
 end
 
 def summon_captain_planet(planeteer_calls)
-  planeteer_calls.map { |calls.upcase| "#{calls}!" }
+  planeteer_calls.map { |calls| "#{calls}!" }
   # Your code here
 end
 
