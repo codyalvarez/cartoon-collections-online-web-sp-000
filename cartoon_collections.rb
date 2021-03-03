27a657690e55379f52059ee0f251ebf85636c521
Cody Alvarez <cody_alvarez19@outlook.com>
2021-03-03 01:52:04 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,8 +1,8 @@
 def roll_call_dwarves(dwarves)
   dwarves.each_with_index { | dwarf, index | puts "#{index + 1}. #{dwarf}" }
-  
+
 end
-# 
+#
 # def roll_call_dwarves(dwarves)
 #   dwarves.each_with_index do | dwarf, index |
 #     puts "#{index + 1}. #{dwarf}"
